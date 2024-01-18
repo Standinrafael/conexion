b0f1c05117dd3e2c0bb0559982e02ec932cbc6f6
Jorge Sanchez <50539993+Standinrafael@users.noreply.github.com>
2024-01-18 10:37:50 -0500

    Id del autor en libro

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "prog-distribuida"
+include("proyecto01")
+include("proyecto02")
+include("cdi01")
+include("jpa01")
+include("cdi-jpa")
+include("cdi-jpa-rest")
+include("examen01-correccion")
+include("app-books")
